1ca3a1f0e8d1249d8121690a15ae1101c2d502ab
Milo Wissig <mwissig@gmail.com>
2019-07-19 14:50:22 -0400

    margin shift

--- app/assets/javascripts/channels/mineplayer.coffee
+++ app/assets/javascripts/channels/mineplayer.coffee
@@ -7,5 +7,10 @@ App.mineplayer = App.cable.subscriptions.create "MineplayerChannel",
 
   received: (data) ->
      unless data.playerid.blank?
-      $('#player' + data.playerid ).css('bottom', data.deltay + 'px')
-      $('#player' + data.playerid ).css('left', data.deltax + 'px')
+      if $('#player' + data.playerid )?
+        $('#player' + data.playerid ).css('bottom', data.deltay + 'px')
+        $('#player' + data.playerid ).css('left', data.deltax + 'px')
+      # else
+      #   $('#minemap ).append  '<div class="player" id="player' + data.playerid + '">New Player<br><span style="color:#000000"><i class="fas fa-walking mineicon"></i></span></div>'
+      #   $('#player' + data.playerid ).css('bottom', data.deltay + 'px')
+      #   $('#player' + data.playerid ).css('left', data.deltax + 'px')
--- app/controllers/mine_controller.rb
+++ app/controllers/mine_controller.rb
@@ -1,24 +1,26 @@
 class MineController < ApplicationController
   def move
+
     ActionCable.server.broadcast 'mineplayer_channel',
       playerid: params[:playerid],
       deltax: params[:deltax],
       deltay: params[:deltay],
       coords: params[:coords]
     head :ok
-@range = (1..5).to_a
-@chance = @range.sample
-if @chance == 1
-    @user = User.find_by(id: params[:playerid])
-    @player = @user.mineplayer
-    @deltax = params[:deltax]
-    @deltay = params[:deltay]
-    @coords = params[:coords]
-    @player.deltax = @deltax
-    @player.deltay = @deltay
-    @player.coords = @coords
-    @player.save!
-end
+
+  @range = (1..5).to_a
+  @chance = @range.sample
+    if @chance == 1
+      @user = User.find_by(id: params[:playerid])
+      @player = @user.mineplayer
+      @deltax = params[:deltax]
+      @deltay = params[:deltay]
+      @coords = params[:coords]
+      @player.deltax = @deltax
+      @player.deltay = @deltay
+      @player.coords = @coords
+      @player.save!
+    end
   end
 
   def dig
